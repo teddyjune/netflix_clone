ad5dadacd71d8401fb2e613d04e6ac13c7563d48
yeonjoo.lee <teddyjune@naver.com>
2022-09-01 23:47:46 +0900

    add: 상단 TopBar 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,72 @@
 import 'package:flutter/material.dart';
-
-import 'main_screen.dart';
+import 'package:netflix_clone/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          brightness: Brightness.dark,
-          primaryColor: Colors.blueGrey),
-      home: const MainScreen(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+        home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              children: [
+                const MainScreen(),
+                Container(),
+                Container(),
+                Container(),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home, color: Colors.white),
+                  label: '홈',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search, color: Colors.white),
+                  label: '검색',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.video_collection_outlined,
+                      color: Colors.white),
+                  label: '공개예정',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.download, color: Colors.white),
+                  label: '저장된 콘텐츠',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.list, color: Colors.white),
+                  label: '더보기',
+                ),
+              ],
+              currentIndex: _selectedIndex,
+              selectedItemColor: Colors.amber[800],
+              onTap: _onItemTapped,
+            ),
+          ),
+        ));
   }
 }
